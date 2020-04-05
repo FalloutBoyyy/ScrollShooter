ef6a1b2cc510ea0143bb0109b123db7910dac6e1
FaloutBoyyy <mailforgit1@gmail.com>
2020-04-05 22:29:10 +0300

    Making a moving spaceship

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+/// Контроллер и поведение игрока ///
+public class Player : MonoBehaviour
+{ ///
+  /// 1 - скорость движения ///
+    public Vector2 speed = new Vector2(20, 20);
+    // 2 - направление движения 
+    private Vector2 movement;
+
+    void Update()
+    { // 3 - извлечь информацию оси
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical"); // 4 - движение в каждом направлении 
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+
+    }
+
+    void FixedUpdate()
+    { // 5 - перемещение игрового объекта 
+
+
+        GetComponent<Rigidbody2D>().velocity = movement;
+
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+
+	public float speed = 10; // скорость пули
+	public Rigidbody2D bullet; // префаб нашей пули
+	public Transform gunPoint; // точка рождения
+	public float fireRate = 1; // скорострельность
+
+
+	// ограничение вращения
+	public float minAngle = -40;
+	public float maxAngle = 40;
+
+	private float curTimeout;
+
+	void Start()
+	{
+	}
+
+	void SetRotation()
+	{
+		Vector3 mousePosMain = Input.mousePosition;
+		mousePosMain.z = Camera.main.transform.position.z;
+		Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePosMain);
+		lookPos = lookPos - transform.position;
+		float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+		angle = Mathf.Clamp(angle, minAngle, maxAngle);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire();
+		}
+		else
+		{
+			curTimeout = 100;
+		}
+	}
+
+	void Fire()
+	{
+		curTimeout += Time.deltaTime;
+		if (curTimeout > fireRate)
+		{
+			curTimeout = 0;
+			Rigidbody2D clone = Instantiate(bullet, gunPoint.position, Quaternion.identity) as Rigidbody2D;
+			clone.velocity = transform.TransformDirection(gunPoint.up * speed);
+			clone.transform.up = gunPoint.up;
+		}
+	}
+}
